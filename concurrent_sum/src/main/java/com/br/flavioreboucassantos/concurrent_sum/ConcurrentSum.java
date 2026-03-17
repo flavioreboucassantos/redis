d12532b2d6dcfdbaeb6678c54c2a6f049a277627
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2026-03-17 13:21:22 -0300

    Creates and Finalize Project concurrent_sum

---
+++ concurrent_sum/src/main/java/com/br/flavioreboucassantos/concurrent_sum/ConcurrentSum.java
@@ -0,0 +1,68 @@
+package com.br.flavioreboucassantos.concurrent_sum;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import redis.clients.jedis.Jedis;
+
+public class ConcurrentSum {
+
+	/*
+	 * Change Here to Select Between runThreadSafeSumTask or runNonThreadSafeSumTask
+	 */
+	static public final boolean isThreadSafe = true;
+
+	/*
+	 * Configurations of Concurrent Sum
+	 */
+	static private final int numberOfAdders = 5;
+	static private final int numberOfSums = 500;
+	static private final long nsTimeBetweenSums = 1000000 * 1; // 1000000 is 1 ms & * 1000 is 1 sec
+
+	static private final AtomicInteger countAddersConcluded = new AtomicInteger(0);
+	static private final AtomicInteger countRollbacks = new AtomicInteger(0);
+
+	static private final String keyName = "valor";
+
+	static private void print(Object p) {
+		System.out.println(p);
+	}
+
+	static private Jedis getNewJedisConnection() {
+		return new Jedis("localhost", 6379);
+	}
+
+	static private boolean allAddersConcluded() {
+		return (countAddersConcluded.get() < numberOfAdders) ? false : true;
+	}
+
+	static public void main(String[] args) throws InterruptedException {
+		Jedis jedis = getNewJedisConnection();
+		jedis.set(keyName, "0");
+		jedis.close();
+
+		for (int i = 0; i < numberOfAdders; i++) {
+			Thread thread = new Thread(new RunnableConcurrentSum(getNewJedisConnection(), keyName, numberOfSums, nsTimeBetweenSums));
+			thread.start();
+		}
+
+		final long msTimeOfStart = System.currentTimeMillis();
+
+		while (!allAddersConcluded())
+			Thread.yield();
+
+		final long msTimeToFinish = System.currentTimeMillis() - msTimeOfStart;
+
+		final int valorEsperado = numberOfAdders * numberOfSums;
+		final int valorEncontrado = Integer.valueOf(jedis.get(keyName));
+
+		print("Tempo gasto: " + msTimeToFinish + " milliseconds");
+		print("Valor esperado: " + valorEsperado);
+		print("Valor encontrado: " + valorEncontrado);
+		print("Rollbacks realizados: " + countRollbacks.get());
+	}
+
+	static public void adderConcluded(final int _countRollbacks) {
+		countRollbacks.addAndGet(_countRollbacks);
+		countAddersConcluded.incrementAndGet();
+	}
+}
---
+++ concurrent_sum/src/main/java/com/br/flavioreboucassantos/concurrent_sum/RunnableConcurrentSum.java
@@ -0,0 +1,123 @@
+package com.br.flavioreboucassantos.concurrent_sum;
+
+import java.util.List;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
+
+public class RunnableConcurrentSum implements Runnable {
+
+	private final int numberOfSums;
+	private final long nsTimeBetweenSums;
+
+	final Jedis jedis;
+	private final String keyName;
+
+	/*
+	 * Non Thread Safe Sum Task
+	 */
+	private void nonThreadSafeSumTask() {
+		int integer = Integer.valueOf(jedis.get(keyName));
+		integer++;
+		jedis.set(keyName, String.valueOf(integer));
+	}
+
+	private void runNonThreadSafeSumTask() {
+		// sum every nsTimeBetweenSums
+		int numberOfSum = 0;
+		while (numberOfSum++ < numberOfSums) {
+			nonThreadSafeSumTask();
+			final long timeNextRun = System.nanoTime() + nsTimeBetweenSums;
+//			System.out.println(numberOfSum + " >>> " + timeNextRun);
+			while (System.nanoTime() < timeNextRun)
+				Thread.yield();
+		}
+		ConcurrentSum.adderConcluded(0);
+	}
+
+	/*
+	 * Thread Safe Sum Task
+	 */
+	private boolean threadSafeSumTask() {
+		/*
+		 * Dica de Outra Abordagem:
+		 * - Usar get dentro de transaction t.get("chave") somente quando precisar do resultado final da operação, pela sequência de realização do jedis.multi()
+		 */
+
+		// Se uma chave monitorada for alterada por outro cliente antes da execução da transação, o Redis aborta a operação, evitando conflitos de concorrência em Java.
+		jedis.watch(keyName); // Monitora desde o jedis.get
+		int integer = Integer.valueOf(jedis.get(keyName));
+		integer++;
+
+		/*
+		 * O comando MULTI serve para iniciar uma transação.
+		 * - Agrupa vários comandos em uma única etapa de execução, garantindo que todos sejam processados sequencialmente, sem a interferência de outros clientes Redis.
+		 * - Atomicidade: O comando MULTI marca o início de um bloco de transação. Após o MULTI, os comandos subsequentes não são executados imediatamente, mas enfileirados.
+		 * - Execução Garantida: Quando o comando EXEC é chamado, todos os comandos da fila são executados. O Redis garante que, se EXEC for chamado, todos os comandos na transação serão executados em ordem.
+		 * - Cancelamento: O comando DISCARD pode ser usado para cancelar a transação, liberando a fila e descartando os comandos enfileirados.
+		 * - Isolamento: Nenhuma outra solicitação de cliente será processada durante a execução da transação (entre MULTI e EXEC), o que garante a consistência dos dados.
+		 */
+		Transaction t = jedis.multi();
+		try {
+			t.set(keyName, String.valueOf(integer));
+
+			List<Object> result = t.exec(); // Se a chave mudou, o exec() retornará null ou vazio.
+
+			if (result == null || result.isEmpty()) // Rollback/Falha: A chave foi alterada por outro cliente.
+				return false;
+
+			// Transação concluída com sucesso!
+			return true;
+		} catch (Exception e) { // Captura erros de erro de lógica ou validação falha.
+			e.printStackTrace();
+			// Use t.discard() para:
+			// - Abortar Transações: Se você iniciou uma série de operações com MULTI e, por algum motivo (erro de lógica, validação falha), decidiu que não deve mais aplicá-las, o DISCARD garante que nada seja alterado.
+			// - Limpar o Buffer da Transação: Remove da fila todos os comandos que foram enviados após o MULTI e antes do EXEC.
+			// - Encerrar o modo MULTI: A conexão retorna ao estado normal, saindo do contexto de transação.
+			t.discard();
+
+			return true; // DISCARDA OPERAÇÃO FALHA - NÂO RETENTA
+		} finally {
+			jedis.unwatch();
+		}
+	}
+
+	private void runThreadSafeSumTask() {
+		// sum every nsTimeBetweenSums
+		int numberOfSum = 0;
+		int countRollbacks = 0;
+		while (numberOfSum++ < numberOfSums) {
+			if (!threadSafeSumTask()) {
+				numberOfSum--;
+				countRollbacks++;
+			}
+			final long timeNextRun = System.nanoTime() + nsTimeBetweenSums;
+//			System.out.println(numberOfSum + " >>> " + timeNextRun);
+			while (System.nanoTime() < timeNextRun)
+				Thread.yield();
+		}
+		ConcurrentSum.adderConcluded(countRollbacks);
+	}
+
+	public RunnableConcurrentSum(Jedis jedis, final String keyName, final int numberOfSums, final long nsTimeBetweenSums) {
+		this.numberOfSums = numberOfSums;
+		this.nsTimeBetweenSums = nsTimeBetweenSums;
+
+		this.jedis = jedis;
+		this.keyName = keyName;
+	}
+
+	@Override
+	public void run() {
+		try {
+
+			if (ConcurrentSum.isThreadSafe)
+				runThreadSafeSumTask();
+			else
+				runNonThreadSafeSumTask();
+
+		} finally {
+			jedis.close();
+		}
+	}
+}
